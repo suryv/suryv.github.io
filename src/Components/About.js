c59d0644fb9551eaff79ca202f68bceea0541f79
suryv <surybur@gmail.com>
2023-04-03 23:32:30 -0700

    add navlink active class toggle. add about me picture

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,16 +6,17 @@ const About = () => {
   return (
     <div className="sub-content about">
       <div className="about-content about-bio">
-        <p>Coming soon!</p>
+        <p>Hi! I'm a UX/UI designer and web developer based in Los Angeles.</p>
         <div className="contact">
-          <div>suryvlobos@gmail.com</div>
+          <div><a href="mailto:suryvlobos@gmail.com">suryvlobos@gmail.com</a></div>
           <div><a href="https://www.linkedin.com/in/sarahivillalobos/" target="_blank" rel="noreferrer">LinkedIn</a></div>
           <div><a href="https://dribbble.com/sury_villalobos" target="_blank" rel="noreferrer">Dribble</a></div>
           <div><a href="https://github.com/vlobos" target="_blank" rel="noreferrer">GitHub</a></div>
         </div>
       </div>
-      <div className="about-content">
+      <div className="about-content about-img">
         <img src={AboutMePic}  alt="about me pic" className="about-me-pic" />
+        <div>This is me at the Pyramids of Giza in Egypt!</div>
       </div>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {  Link } from "react-router-dom";
+import {  NavLink,Link } from "react-router-dom";
+
 const header= () =>{
   return (
   <div className="header">
     <div className="header-name"><Link to="/Portfolio"><p>SURY</p><p>VILLALOBOS</p></Link></div>
       <div className="nav-bar">
-        <Link to="/Portfolio">Portfolio</Link>
-        <Link to="/About">About</Link>
+        <NavLink to="/Portfolio" exact activeClassName='is-active' className="nav-p">Portfolio</NavLink>
+        <NavLink to="/About" activeClassName='is-active' className="nav-a">About</NavLink>
         {/* <Link to="/Contact">Contact</Link> */}
       </div>
   </div>
